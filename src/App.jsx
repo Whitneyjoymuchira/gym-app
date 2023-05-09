7278fa8a90f885537314791cede256e8dc81fa2c
Whitney Muchira <whitneymuchira@gmail.com>
2023-05-09 16:00:40 +0300

    website initialiazation

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./App.css"
+import { Box } from '@mui/material'
+import { Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import ExerciseDetail from './Pages/ExerciseDetail'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+  <Box width="400px">
+<Navbar />
+<Routes>
+  <Route path='/'  element={<Home />}/>
+  <Route path="/exercise/:id" element={<ExerciseDetail />} />
+</Routes>
+<Footer />
+  </Box>
+
+  )
+}
+
+export default App
